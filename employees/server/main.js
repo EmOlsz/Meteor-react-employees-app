6e7a1340a62649d7b08b7dd7da22cd11d87238f7
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-03-15 17:12:46 +0200

    imported newly created collection

--- employees/server/main.js
+++ employees/server/main.js
@@ -1,8 +1,9 @@
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
 
 Meteor.startup(() => {
   const numberRecords = Employees.find({}).count();
-  if (!numberRecords) {
-    
-  }
+  // if (!numberRecords) {
+  //
+  // }
 });
