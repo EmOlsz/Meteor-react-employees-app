1cf49468f1a9203d23d8dbca9333317ec6de0de9
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-03-17 12:39:51 +0200

    Added loop to insert records to db

--- employees/server/main.js
+++ employees/server/main.js
@@ -1,9 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 import { Employees } from '../imports/collections/employees';
+import { image, helpers } from 'faker';
+import _ from 'lodash';
 
 Meteor.startup(() => {
   const numberRecords = Employees.find({}).count();
-  // if (!numberRecords) {
-  //
-  // }
+  console.log(numberRecords);
+  if (!numberRecords) {
+    _.times(5000, () => {
+      const { name, email, phone } = helpers.createCard();
+      const avatar = image.avatar();-
+
+      Employees.insert({
+        name,
+        email,
+        phone,
+        avatar
+      })
+    });
+  }
 });
